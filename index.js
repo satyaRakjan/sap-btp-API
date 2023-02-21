06c6fd78b606c318e92970e8455aed635a4e5788
satyaRakjan <rakjan.sattaya@gmail.com>
2023-02-21 12:07:29 +0700

    update core

--- index.js
+++ index.js
@@ -22,7 +22,11 @@ app.use(bodyParser.json());
 //   credentials: true,
 // };
 // app.use(cors(corsOptions));
-
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*"); //หรือใส่แค่เฉพาะ domain ที่ต้องการได้
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
